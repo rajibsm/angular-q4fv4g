8a65037a69d99d7f9be3eaffcb57402bd5b305dc
rajibsm <rajib.miah@gmail.com>
2020-07-09 16:26:16 -0500

    Added shipping

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+items;
+checkoutForm;
+
+  constructor(
+      private cartService: CartService,
+      private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+     this.items = this.cartService.getItems();
+  }
+
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -9,11 +10,18 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  
   product;
   
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
+
+   addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
